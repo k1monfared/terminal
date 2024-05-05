c399978bb203c13de85384ed1b3376c9d6b9260b
Keivan <k1monfared@gmail.com>
2024-05-04 20:21:46 -0700

    quick commit

--- bash_scripts/git_switch.sh
+++ bash_scripts/git_switch.sh
@@ -4,15 +4,15 @@ account=$1
 if [ $account = "l2l" ]
 then
     git config user.email "langroudlangford@gmail.com"
-    git config user.name  "l2l"
+    git config user.name "l2l"
 elif [ $account = "k1monfared" ]
 then
     git config user.email "k1monfared@gmail.com"
-    git config user.name  "Keivan"
+    git config user.name "Keivan"
 elif [ $account = "photo" ]
 then
     git config user.email "k1monfaredphoto@gmail.com"
-    git config user.name  "Keivan"
+    git config user.name "Keivan"
 else
     echo "The account $1 does not exist!"
 fi
