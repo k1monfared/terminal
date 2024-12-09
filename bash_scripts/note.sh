c6b7c23401f5154d7c82582f98e4b4424889787d
Keivan <k1monfared@gmail.com>
2024-12-08 19:00:29 -0800

    quick commit

--- bash_scripts/note.sh
+++ bash_scripts/note.sh
@@ -2,29 +2,12 @@
 
 ## date format ##
 DATETIME=$(date +"%Y%m%d_%H%M%S")
-
 ## Save path ##
 BAK="$HOME/Documents/drafts"
 mkdir -p -- "$BAK"
 filename="note_$DATETIME"
 FILE="$BAK/$filename"
-# open the editor to add text
-pulsar $FILE 2>/dev/null & # The 2>/dev/null part ignores the error messages in the command prompt output. It is assuming /dev/null exists though!
+pulsar $FILE 2>/dev/null &
 echo "file is saved at the following address:"
-echo $FILE #this will echo the file name and address in the terminal so that I know the name of it if I need it and that I know everything worked fine.
+echo $FILE
 echo $FILE >> $BAK/list
-
-# if you want to add functionality to rename the file afterwards
-# read -p "do you want to change the name of the file? (y/[n]) " response
-#
-# case $response in
-#     [Yy]* )
-#         read -p "Enter new file name: " filename
-#         NEWFILE="$BAK/$filename"
-#         mv $FILE $NEWFILE
-#         echo $NEWFILE
-#         echo $NEWFILE >> $BAK/list #this adds the name of the file to the list of the files. I'm going to read that list later to access the last file, or one before last file, and so on!
-#     ;;
-#     * )
-#         echo $FILE >> $BAK/list
-# esac
--- bash_scripts/searchdrafts.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-STRING=$*
-BACK="/home/$USER/Documents/drafts"
-#echo $STRING
-grep -irn --color=always "$STRING" $BACK | grep -v "~"
