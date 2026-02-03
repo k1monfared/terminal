b57689f31256d3fce812db5b257025b87912c5c1
Keivan <k1monfared@gmail.com>
2026-02-02 22:12:01 -0800

    quick commit

--- bash_scripts/blog.sh
+++ bash_scripts/blog.sh
@@ -4,9 +4,10 @@
 DATETIME=$(date +"%Y%m%d_")
 ## Save path ##
 mkdir -p -- "$blog_folder"
-filename="note_$DATETIME"
-FILE="$blog_folder/$filename"
-code $FILE 2>/dev/null &
+filename="$DATETIME"
+FILE="$blog_folder$filename"
+echo $EDITOR
+$EDITOR $FILE 2>/dev/null &
 echo "file is saved at the following address:"
 echo $FILE
 echo $FILE >> $blog_folder/list
